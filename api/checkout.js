ab67f9dcef7cc82cce7ad9942f038e24ff11a2c4
Techonni <20517985+techonni@users.noreply.github.com>
2026-09-17 22:17:25 +0100

    Enable Stripe invoice on checkout sessions

--- api/checkout.js
+++ api/checkout.js
@@ -40,7 +40,9 @@ module.exports = async (req, res) => {
       line_items: [{ price: product.price, quantity: 1 }],
       success_url: origin + product.path + "?paid=1",
       cancel_url: origin + product.path,
-      managed_payments: { enabled: false },
+      customer_creation: "always",
+      invoice_creation: { enabled: true },
+      billing_address_collection: "auto",
     });
     res.writeHead(303, { Location: session.url });
     res.end();
