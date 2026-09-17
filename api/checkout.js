ed4de31ff0c62a2c30fec96c255bbf35fadce4cd
Techonni <20517985+techonni@users.noreply.github.com>
2026-09-17 22:10:51 +0100

    Disable managed payments on Checkout Session

--- api/checkout.js
+++ api/checkout.js
@@ -40,6 +40,7 @@ module.exports = async (req, res) => {
       line_items: [{ price: product.price, quantity: 1 }],
       success_url: origin + product.path + "?paid=1",
       cancel_url: origin + product.path,
+      managed_payments: { enabled: false },
     });
     res.writeHead(303, { Location: session.url });
     res.end();
